18d836fec0ba08faba3ac5ff34333a282a895f1b
ライアン <ryan.wachowski@duethealth.com>
2020-06-18 14:18:04 -0400

    WIP: patching package file so that SwiftUI previewing works

--- Package.swift
+++ Package.swift
@@ -7,15 +7,15 @@ let package = Package(
     platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         .library(name: "Hexicon", targets: ["Hexicon"]),
-        .executable(name: "hexiconj", targets: ["Hexiconjuror"]),
+        // .executable(name: "hexiconj", targets: ["Hexiconjuror"]),
     ],
     dependencies: [
-        .package(name: "Commandant", url: "https://github.com/carthage/commandant", from: "0.17.0"),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50200.0")),
+        // .package(name: "Commandant", url: "https://github.com/carthage/commandant", from: "0.17.0"),
+        // .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax", .exact("0.50200.0")),
     ],
     targets: [
         .target(name: "Hexicon", dependencies: []),
-        .target(name: "Hexiconjuror", dependencies: ["Hexicon", "Commandant", "SwiftSyntax"]),
-        .testTarget(name: "HexiconTests", dependencies: ["Hexicon"]),
+        // .target(name: "Hexiconjuror", dependencies: ["Hexicon", "Commandant", "SwiftSyntax"]),
+        // .testTarget(name: "HexiconTests", dependencies: ["Hexicon"]),
     ]
 )
